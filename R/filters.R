60700f2e5ccb87fe6c0507e5cf29efef3fb01b19
AQLT <alain.quartier@yahoo.fr>
2020-06-19 10:53:40 +0200

    plot functions and properties filters

--- R/filters.R
+++ R/filters.R
@@ -50,23 +50,67 @@ localpolynomials<-function(y, horizon, degree=3, kernel=c("Henderson", "Uniform"
 #' @export
 #'
 #' @examples
-filterproperties<-function(horizon, degree=3, kernel=c("Henderson", "Uniform", "Biweight", "Triweight", "Tricube", "Gaussian", "Triangular", "Parabolic"), endpoints=c("LC", "QL", "CQ", "CC", "DAF"), ic=4.5){
+filterproperties <- function(horizon, degree=3,
+                           kernel=c("Henderson", "Uniform", "Biweight", "Triweight", "Tricube", "Gaussian", "Triangular", "Parabolic"),
+                           endpoints=c("LC", "QL", "CQ", "CC", "DAF"),
+                           ic=4.5){
   d<-2/(sqrt(pi)*ic)
   kernel=match.arg(kernel)
   endpoints=match.arg(endpoints)
-  jprops<-.jcall("demetra/saexperimental/r/LocalPolynomialFilters", "Ldemetra/saexperimental/r/FiltersToolkit$FiniteFilters;", "filterProperties", as.integer(horizon), as.integer(degree), kernel, endpoints, d)
+  jprops<-.jcall("demetra/saexperimental/r/LocalPolynomialFilters",
+                 "Ldemetra/saexperimental/r/FiltersToolkit$FiniteFilters;",
+                 "filterProperties", as.integer(horizon),
+                 as.integer(degree), kernel, endpoints, d)
   sw<-proc_data(jprops, "sweights")
   swg<-proc_data(jprops, "sgain")
-  aw<-sapply(1:horizon, function(h){return(proc_data(jprops, paste0("aweights(", horizon-h,')')))})
-  awg<-sapply(1:horizon, function(h){return(proc_data(jprops, paste0("again(", horizon-h,')')))})
-  awp<-sapply(1:horizon, function(h){return(proc_data(jprops, paste0("aphase(", horizon-h,')')))})
+  aw<-sapply(0:(horizon-1), function(h){return(proc_data(jprops, paste0("aweights(", h,')')))})
+  awg<-sapply(0:(horizon-1), function(h){return(proc_data(jprops, paste0("again(", h,')')))})
+  awp<-sapply(0:(horizon-1), function(h){return(proc_data(jprops, paste0("aphase(", h,')')))})
+  coefs = c(aw,list(sw))
+  nbpoints = horizon*2+1
+  coefs = sapply(coefs, function(x){
+    c(x,rep(0,nbpoints-length(x)))
+  })
+  gain = cbind(awg,swg)
+  filternames <- sprintf("q=%i", 0:(horizon))
+  rownames(coefs) <- sprintf("t%+i", seq(-horizon,horizon))
+  rownames(coefs) <- sub("+0", "", rownames(coefs), fixed = TRUE)
+  colnames(gain) <- colnames(coefs) <- filternames
+  colnames(awp) <- filternames[-length(filternames)]
+
   return(structure(list(
     internal=jprops,
-    symmetricFilter=sw,
-    symmetricfilter.gain=swg,
-    asymmetricFilters=aw,
-    asymmetricfilter.gain=awg,
+    filters.coef = coefs,
+    filters.gain = gain,
     asymmetricfilter.phase=awp
-    ),
-    class="JD.Filters"))
+  ),
+  class="JD.Filters"))
 }
+# filterproperties<-function(horizon, degree=3,
+#                            kernel=c("Henderson", "Uniform", "Biweight", "Triweight", "Tricube", "Gaussian", "Triangular", "Parabolic"),
+#                            endpoints=c("LC", "QL", "CQ", "CC", "DAF"),
+#                            ic=4.5){
+#   d<-2/(sqrt(pi)*ic)
+#   kernel=match.arg(kernel)
+#   endpoints=match.arg(endpoints)
+#   jprops<-.jcall("demetra/saexperimental/r/LocalPolynomialFilters",
+#                  "Ldemetra/saexperimental/r/FiltersToolkit$FiniteFilters;",
+#                  "filterProperties", as.integer(horizon),
+#                  as.integer(degree), kernel, endpoints, d)
+#   sw<-proc_data(jprops, "sweights")
+#   swg<-proc_data(jprops, "sgain")
+#   asymetric.names <- sprintf("q=%i", 0:(horizon-1))
+#   aw<-sapply(0:(horizon-1), function(h){return(proc_data(jprops, paste0("aweights(", h,')')))})
+#   awg<-sapply(0:(horizon-1), function(h){return(proc_data(jprops, paste0("again(", h,')')))})
+#   awp<-sapply(0:(horizon-1), function(h){return(proc_data(jprops, paste0("aphase(", h,')')))})
+#   names(aw) <- colnames(awg) <- colnames(awp) <- asymetric.names
+#   return(structure(list(
+#     internal=jprops,
+#     symmetricFilter.coef=sw,
+#     symmetricfilter.gain=swg,
+#     asymmetricFilters.coef=aw,
+#     asymmetricfilter.gain=awg,
+#     asymmetricfilter.phase=awp
+#   ),
+#   class="JD.Filters"))
+# }
---
+++ R/plots.R
@@ -0,0 +1,77 @@
+#' Plots
+#'
+#' @param x coefficients, gain or phase
+#' @param q q
+#' @param nxlab number of xlab
+#' @param ... other arguments to \code{plot}
+#' @param add add to previous plot
+#'
+#' @return
+#'
+#' @examples
+#' @name plot_filters
+#' @rdname plot_filters
+#' @export
+plot_coef <- function(x, q = 0, add = FALSE,...){
+  x_values = seq(0, nrow(x)) - (nrow(x) - 1)/2
+  y = x[, sprintf("q=%i",q)]
+  if(!add){
+    plot(y, type = "l",
+         xaxt = "n", xlab = "",
+         ylab = "coefficient", ...)
+    axis(1, at=seq_len(nrow(x)), labels = rownames(x))
+  }else{
+    lines(y, ...)
+  }
+}
+#' @name plot_filters
+#' @rdname plot_filters
+#' @export
+plot_gain <- function(x, q = 0, nxlab = 6, add = FALSE,...){
+  x_values = seq(0, 2 * pi, length.out = nrow(x))
+  y = x[,sprintf("q=%i", q)]
+  if(!add){
+    plot(x_values, y, type = "l",
+         xaxt = "n", xlab = "",
+         ylab = "gain", ...)
+    x_lab_at <- seq(0, 2, length.out = nxlab)
+    axis(1, at = x_lab_at * pi, labels = xlabel(x_lab_at))
+  }else{
+    lines(x_values, y, ...)
+  }
+}
+#' @name plot_filters
+#' @rdname plot_filters
+#' @export
+plot_phase <- function(x, q = 0, nxlab = 6, add = FALSE,...){
+  x_values = seq(0, 2 * pi, length.out = nrow(x))
+
+  if(q < ncol(x))
+    y = x[,sprintf("q=%i",q)]
+
+  if(!add){
+    if(q >= ncol(x)){
+      plot(1, type="n",xaxt = "n", xlab = "",
+           ylab = "phase", xlim=c(0, 2*pi), ylim=c(0, 1),
+           ...)
+    }else{
+      plot(x_values, y, type = "l",
+           xaxt = "n", xlab = "",
+           ylab = "phase", ...)
+    }
+
+    x_lab_at <- seq(0, 2, length.out = nxlab)
+    axis(1, at = x_lab_at * pi, labels = xlabel(x_lab_at))
+  }else{
+    lines(x_values, y, ...)
+  }
+}
+
+xlabel <- function(x, symbol = "pi"){
+  fracs <- strsplit(attr(MASS::fractions(x), "fracs"), "/")  # convert to fractions
+  labels <- sapply(fracs, function(i)
+    if (length(i) > 1) { paste(i[1], "*", symbol, "/", i[2]) }
+    else { paste(i, "*", symbol) })
+  labels[1] <- "0"
+  parse(text = labels)
+}
