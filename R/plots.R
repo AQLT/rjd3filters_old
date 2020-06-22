9cb393a2c01d0ae4840c5f66c0b558d7f4fc7392
AQLT <alain.quartier@yahoo.fr>
2020-06-22 10:44:05 +0200

    plot correction

--- R/plots.R
+++ R/plots.R
@@ -10,12 +10,22 @@
 #'
 #'
 #' @examples
+#' filter <- filterproperties(3, endpoints = "DAF", kernel = "Henderson")
+#' plot_coef(filter, q = c(0,3), legend = TRUE)
+#' plot_gain(filter, q = c(0,3), legend = TRUE)
+#' plot_phase(filter, q = c(0,3), legend = TRUE)
 #' @name plot_filters
 #' @rdname plot_filters
 #' @importFrom MASS fractions
 #' @export
 plot_coef <- function(x, q = 0, add = FALSE, legend = FALSE,
                       legend.pos = "topright", ...){
+  UseMethod("plot_coef", x)
+}
+#' @export
+plot_coef.JD.Filters <- function(x, q = 0, nxlab = 7, add = FALSE, legend = FALSE,
+                                 legend.pos = "topright", ...){
+  x = f$filters.coef
   col_to_plot <- sprintf("q=%i",q)
   col_to_plot <- col_to_plot[col_to_plot %in% colnames(x)]
   if (length(col_to_plot) == 0) {
@@ -41,7 +51,13 @@ plot_coef <- function(x, q = 0, add = FALSE, legend = FALSE,
 #' @export
 plot_gain <- function(x, q = 0, nxlab = 7, add = FALSE, legend = FALSE,
                       legend.pos = "topright", ...){
-  x_values = seq(0, 2 * pi, length.out = nrow(x))
+  UseMethod("plot_gain", x)
+}
+#' @export
+plot_gain.JD.Filters <- function(x, q = 0, nxlab = 7, add = FALSE, legend = FALSE,
+                      legend.pos = "topright", ...){
+  x = x$filters.gain
+  x_values = seq(0, pi, length.out = nrow(x))
   col_to_plot <- sprintf("q=%i",q)
   col_to_plot <- col_to_plot[col_to_plot %in% colnames(x)]
   if (length(col_to_plot) == 0) {
@@ -49,7 +65,7 @@ plot_gain <- function(x, q = 0, nxlab = 7, add = FALSE, legend = FALSE,
       plot(1, type="n",xaxt = "n", xlab = "",
            ylab = "gain", xlim=c(0, 2*pi), ylim=c(0, 1),
            ...)
-      x_lab_at <- seq(0, 2, length.out = nxlab)
+      x_lab_at <- seq(0, 1, length.out = nxlab)
       axis(1, at = x_lab_at * pi, labels = xlabel(x_lab_at))
     }
     return(invisible(0))
@@ -62,16 +78,22 @@ plot_gain <- function(x, q = 0, nxlab = 7, add = FALSE, legend = FALSE,
     legend(legend.pos,col_to_plot,
            col = seq_along(col_to_plot), lty=seq_along(col_to_plot), lwd=2)
   if(!add){
-    x_lab_at <- seq(0, 2, length.out = nxlab)
+    x_lab_at <- seq(0, 1, length.out = nxlab)
     axis(1, at = x_lab_at * pi, labels = xlabel(x_lab_at))
   }
 }
 #' @name plot_filters
 #' @rdname plot_filters
 #' @export
-plot_phase <- function(x, q = 0, nxlab = 6, add = FALSE, legend = FALSE,
-                       legend.pos = "topright",...){
-  x_values = seq(0, 2 * pi, length.out = nrow(x))
+plot_phase <- function(x, q = 0, nxlab = 7, add = FALSE, legend = FALSE,
+                       legend.pos = "topright", ...){
+  UseMethod("plot_phase", x)
+}
+#' @export
+plot_phase.JD.Filters <- function(x, q = 0, nxlab = 7, add = FALSE, legend = FALSE,
+                       legend.pos = "topright", ...){
+  x = x$asymmetricfilter.phase
+  x_values = seq(0, pi, length.out = nrow(x))
   col_to_plot <- sprintf("q=%i",q)
   col_to_plot <- col_to_plot[col_to_plot %in% colnames(x)]
   if (length(col_to_plot) == 0) {
@@ -79,7 +101,7 @@ plot_phase <- function(x, q = 0, nxlab = 6, add = FALSE, legend = FALSE,
       plot(1, type="n",xaxt = "n", xlab = "",
            ylab = "phase", xlim=c(0, 2*pi), ylim=c(0, 1),
            ...)
-      x_lab_at <- seq(0, 2, length.out = nxlab)
+      x_lab_at <- seq(0, 1, length.out = nxlab)
       axis(1, at = x_lab_at * pi, labels = xlabel(x_lab_at))
     }
     return(invisible(0))
@@ -91,7 +113,7 @@ plot_phase <- function(x, q = 0, nxlab = 6, add = FALSE, legend = FALSE,
     legend(legend.pos,col_to_plot,
            col = seq_along(col_to_plot), lty=seq_along(col_to_plot), lwd=2)
   if(!add){
-    x_lab_at <- seq(0, 2, length.out = nxlab)
+    x_lab_at <- seq(0, 1, length.out = nxlab)
     axis(1, at = x_lab_at * pi, labels = xlabel(x_lab_at))
   }
 
