317a0c1214e64e54bb631caf4378f920afc178ec
AQLT <alain.quartier@yahoo.fr>
2020-06-27 16:35:43 +0200

    xlim and normalized phase shift function

--- R/plots.R
+++ R/plots.R
@@ -4,10 +4,11 @@
 #' @param q q
 #' @param nxlab number of xlab
 #' @param ... other arguments to \code{matplot}
-#' @param add boolean indicating if the new plot is added to the previous one
-#' @param legend boolean indicating if the legend is printed
-#' @param legend.pos position of the legend
-#'
+#' @param add boolean indicating if the new plot is added to the previous one.
+#' @param xlim vector containing x limits.
+#' @param legend boolean indicating if the legend is printed.
+#' @param legend.pos position of the legend.
+#' @param normalized boolean indicatif if the phase function is normalized by the frequency.
 #'
 #' @examples
 #' filter <- filterproperties(3, endpoints = "DAF", kernel = "Henderson")
@@ -50,12 +51,12 @@ plot_coef.JD.Filters <- function(x, q = 0, nxlab = 7, add = FALSE, legend = FALS
 #' @rdname plot_filters
 #' @export
 plot_gain <- function(x, q = 0, nxlab = 7, add = FALSE, legend = FALSE,
-                      legend.pos = "topright", ...){
+                      legend.pos = "topright", xlim = c(0, pi), ...){
   UseMethod("plot_gain", x)
 }
 #' @export
 plot_gain.JD.Filters <- function(x, q = 0, nxlab = 7, add = FALSE, legend = FALSE,
-                      legend.pos = "topright", ...){
+                      legend.pos = "topright", xlim = c(0, pi), ...){
   x = x$filters.gain
   x_values = seq(0, pi, length.out = nrow(x))
   col_to_plot <- sprintf("q=%i",q)
@@ -63,22 +64,22 @@ plot_gain.JD.Filters <- function(x, q = 0, nxlab = 7, add = FALSE, legend = FALS
   if (length(col_to_plot) == 0) {
     if(!add){
       plot(1, type="n",xaxt = "n", xlab = "",
-           ylab = "gain", xlim=c(0, pi), ylim=c(0, 1),
+           ylab = "gain", xlim=xlim, ylim=c(0, 1),
            ...)
-      x_lab_at <- seq(0, 1, length.out = nxlab)
+      x_lab_at <- seq(xlim[1]/pi, xlim[2]/pi, length.out = nxlab)
       axis(1, at = x_lab_at * pi, labels = xlabel(x_lab_at))
     }
     return(invisible(0))
   }
   matplot(x_values,x[, col_to_plot], type = "l",
           xaxt = "n", xlab = "",
-          ylab = "gain", add = add, ...)
+          ylab = "gain", add = add, xlim = xlim, ...)
 
   if(legend)
     legend(legend.pos,col_to_plot,
            col = seq_along(col_to_plot), lty=seq_along(col_to_plot), lwd=2)
   if(!add){
-    x_lab_at <- seq(0, 1, length.out = nxlab)
+    x_lab_at <- seq(xlim[1]/pi, xlim[2]/pi, length.out = nxlab)
     axis(1, at = x_lab_at * pi, labels = xlabel(x_lab_at))
   }
 }
@@ -86,34 +87,37 @@ plot_gain.JD.Filters <- function(x, q = 0, nxlab = 7, add = FALSE, legend = FALS
 #' @rdname plot_filters
 #' @export
 plot_phase <- function(x, q = 0, nxlab = 7, add = FALSE, legend = FALSE,
-                       legend.pos = "topright", ...){
+                       legend.pos = "topright", xlim = c(0, pi), normalized = TRUE, ...){
   UseMethod("plot_phase", x)
 }
 #' @export
 plot_phase.JD.Filters <- function(x, q = 0, nxlab = 7, add = FALSE, legend = FALSE,
-                       legend.pos = "topright", ...){
-  x = x$asymmetricfilter.phase
+                       legend.pos = "topright", xlim = c(0, pi), normalized = TRUE, ...){
+  x = x$filters.phase
   x_values = seq(0, pi, length.out = nrow(x))
+  if(normalized){
+    x[-1,] <- x[-1,] / x_values[-1]
+  }
   col_to_plot <- sprintf("q=%i",q)
   col_to_plot <- col_to_plot[col_to_plot %in% colnames(x)]
   if (length(col_to_plot) == 0) {
     if(!add){
       plot(1, type="n",xaxt = "n", xlab = "",
-           ylab = "phase", xlim=c(0, pi), ylim=c(0, 1),
+           ylab = "phase", xlim = xlim, ylim=c(0, 1),
            ...)
-      x_lab_at <- seq(0, 1, length.out = nxlab)
+      x_lab_at <- seq(xlim[1]/pi, xlim[2]/pi, length.out = nxlab)
       axis(1, at = x_lab_at * pi, labels = xlabel(x_lab_at))
     }
     return(invisible(0))
   }
   matplot(x_values,x[, col_to_plot], type = "l",
           xaxt = "n", xlab = "",
-          ylab = "phase", add = add)
+          ylab = "phase", add = add, xlim = xlim, ...)
   if(legend)
     legend(legend.pos,col_to_plot,
            col = seq_along(col_to_plot), lty=seq_along(col_to_plot), lwd=2)
   if(!add){
-    x_lab_at <- seq(0, 1, length.out = nxlab)
+    x_lab_at <- seq(xlim[1]/pi, xlim[2]/pi, length.out = nxlab)
     axis(1, at = x_lab_at * pi, labels = xlabel(x_lab_at))
   }
 
@@ -124,7 +128,7 @@ xlabel <- function(x, symbol = "pi"){
   labels <- sapply(fracs, function(i)
     if (length(i) > 1) { paste(i[1], "*", symbol, "/", i[2]) }
     else { paste(i, "*", symbol) })
-  labels[1] <- "0"
+  labels <- sub("0 * pi", "0", labels, fixed = TRUE)
   labels <- sub("1 * pi", " pi", labels, fixed = TRUE)
   parse(text = labels)
 }
