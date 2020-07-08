ad6a057a2d644e1f60046fadf0edd49fb2f818a6
AQLT <alain.quartier@yahoo.fr>
2020-07-08 18:20:47 +0200

    PR update

--- R/filters.R
+++ R/filters.R
@@ -167,31 +167,79 @@ filterproperties <- function(horizon, degree = 3,
   ),
   class="JD.Filters"))
 }
-# filterproperties<-function(horizon, degree=3,
-#                            kernel=c("Henderson", "Uniform", "Biweight", "Triweight", "Tricube", "Gaussian", "Triangular", "Parabolic"),
-#                            endpoints=c("LC", "QL", "CQ", "CC", "DAF"),
-#                            ic=4.5){
-#   d<-2/(sqrt(pi)*ic)
-#   kernel=match.arg(kernel)
-#   endpoints=match.arg(endpoints)
-#   jprops<-.jcall("demetra/saexperimental/r/LocalPolynomialFilters",
-#                  "Ldemetra/saexperimental/r/FiltersToolkit$FiniteFilters;",
-#                  "filterProperties", as.integer(horizon),
-#                  as.integer(degree), kernel, endpoints, d)
-#   sw<-proc_data(jprops, "sweights")
-#   swg<-proc_data(jprops, "sgain")
-#   asymetric.names <- sprintf("q=%i", 0:(horizon-1))
-#   aw<-sapply(0:(horizon-1), function(h){return(proc_data(jprops, paste0("aweights(", h,')')))})
-#   awg<-sapply(0:(horizon-1), function(h){return(proc_data(jprops, paste0("again(", h,')')))})
-#   awp<-sapply(0:(horizon-1), function(h){return(proc_data(jprops, paste0("aphase(", h,')')))})
-#   names(aw) <- colnames(awg) <- colnames(awp) <- asymetric.names
-#   return(structure(list(
-#     internal=jprops,
-#     symmetricFilter.coef=sw,
-#     symmetricfilter.gain=swg,
-#     asymmetricFilters.coef=aw,
-#     asymmetricfilter.gain=awg,
-#     asymmetricfilter.phase=awp
-#   ),
-#   class="JD.Filters"))
-# }
+
+
+#' Estimation of a filter using the Fidelity-Smoothness-Timeliness criterions
+#'
+#' @param lags Lags of the filter (should be positive)
+#' @param leads Leads of the filter (should be positive or 0)
+#' @param pdegree Local polynomials preservation: max degree
+#' @param passband Passband of the filter (only used if tweight is )
+#' @param smoothness.weight Weight for the smoothness criterion (in [0, 1])
+#' @param smoothness.degree Degree of the smoothness criterion (3 for Henderson)
+#' @param timeliness.weight Weight for the Timeliness criterion (in [0, 1[). sweight+tweight should be in [0,1]
+#' @param timeliness.passband Passband for the timeliness criterion (in radians). The phase effect is computed in [0, passband]
+#' @param timeliness.antiphase See Guggemos
+#'
+#' @return Contains the selected filter, its gain and phase, and the values of the 3 criterions
+#' @export
+#'
+#' @examples
+fstfilter<-function(lags, leads, pdegree=2, smoothness.weight=1, smoothness.degree=3, timeliness.weight=0, timeliness.passband=pi/6, timeliness.antiphase=T){
+  jobj<-.jcall("demetra/saexperimental/r/FiltersToolkit", "Ldemetra/saexperimental/r/FiltersToolkit$FSTResult;",
+               "fstfilter", as.integer(lags), as.integer(leads), as.integer(pdegree), smoothness.weight, as.integer(smoothness.degree),
+              timeliness.weight, timeliness.passband, as.logical(timeliness.antiphase))
+  return(fstresult(jobj))
+
+}
+
+fstresult<-function(jobj){
+  filter<-.jcall(jobj,"[D", "weights")
+  gain<-.jcall(jobj, "[D", "getGain")
+  phase<-.jcall(jobj, "[D", "getPhase")
+  criterions<-.jcall(jobj, "[D", "getCriterions")
+
+  return(list(
+    filter=filter,
+    gain=gain,
+    phase=phase,
+    criterions=criterions
+  ))
+}
+
+#' FST criterions
+#'
+#' @param weights Weights of the filter (from lower bound to upper bound)
+#' @param lb Lower bound (usually negative)
+#' @param passband Passband threshold for timeliness criterion
+#'
+#' @return
+#' @export
+#'
+#' @examples
+fst<-function(weights, lb, passband=pi/6){
+  jobj<-.jcall("demetra/saexperimental/r/FiltersToolkit", "Ldemetra/saexperimental/r/FiltersToolkit$FSTResult;", "fst",
+               weights, as.integer(lb), passband)
+               return(fstresult(jobj))
+
+}
+
+#' Accuracy/smoothness/timeliness criterions through spectral decomposition
+#' See Wildi/McElroy
+#'
+#' @param sweights Weights of the symmetric filter (from 0 to n only).
+#' @param aweights Weights of the asymmetric filter (from -n to m)
+#' @param density hypothesis on the spectral density
+#' @param passband passband threshold
+#'
+#' @return The criterions
+#' @export
+#'
+#' @examples
+mse<-function(sweights, aweights, density=c("rw", "uniform"), passband=pi/6 ){
+  spectral=match.arg(spectral)
+  rslt<-.jcall("demetra/saexperimental/r/FiltersToolkit", "[D", "mseDecomposition",
+                 sweights, aweights, spectral, passband)
+  return (list(accuracy=rslt[1], smoothness=rslt[2], timeliness=rslt[3], residual=rslt[4]))
+}
+
