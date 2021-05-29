df67f73ff281e38bfe917b0a56ee11e54b434664
AQLT <alain.quartier@yahoo.fr>
2021-05-29 17:02:21 +0200

    change print method

--- R/print.R
+++ R/print.R
@@ -1,15 +1,15 @@
 #' @export
 print.fst_filter <- function(x, ...){
-  print("Coefficients of FST filter:\n")
+  cat("$filters.coef\n")
   print(x$filters.coef, ...)
 }
 #' @export
 print.lp_filter <- function(x, ...){
-  print("Coefficients of Local Polynomial filter:\n")
+  cat("$filters.coef\n")
   print(x$filters.coef, ...)
 }
 #' @export
 print.rkhs_filter <- function(x, ...){
-  cat("Coefficients of the RKHS filter:\n")
+  cat("$filters.coef\n")
   print(x$filters.coef, ...)
 }
