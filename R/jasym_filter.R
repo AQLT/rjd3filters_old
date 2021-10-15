ec7d0b81e386c3914a6270e06d28264e9607d40e
AQLT <alain.quartier@yahoo.fr>
2021-10-15 11:36:27 +0200

    jasym applied using FiniteFilters

--- R/jasym_filter.R
+++ R/jasym_filter.R
@@ -3,7 +3,9 @@
 #' Applies linear filtering to a univariate time series or to each series separately of a multivariate time series using an asymmetric moving average
 #'
 #' @param y a univariate or multivariate time series.
-#' @param coefs the coefficients of the asymmetric moving average.
+#' @param coefs an object of class \code{"moving_average"} or the coefficients
+#' of a moving average. In that case the argument \code{lags} must be provided.
+#' @inheritParams fst_filter
 #'
 #' @details
 #' For a time series \eqn{x} the asymmetric moving average is applied.
@@ -25,55 +27,74 @@
 #'
 #' plot(ts.union(y, fst_ma, lpp_ma), plot.type = "single", col = c("black","red","blue"))
 #' @seealso \code{\link{jfilter}}
-#' @inheritParams fst_filter
 #' @export
 jasym_filter <- function(y, coefs, lags){
   UseMethod("jasym_filter", y)
 }
 #' @export
 jasym_filter.default <- function(y, coefs, lags){
-  coefs <- removeTrailingZeroOrNA(coefs)
-  leads <- length(coefs)-(lags+1)
-  jsymf <- .jcall("jdplus/math/linearfilters/FiniteFilter",
-                  "Ljdplus/math/linearfilters/FiniteFilter;",
-                  "of", coefs,as.integer(-lags))
-  jsymf <- .jcast(jsymf, "jdplus/math/linearfilters/IFiniteFilter")
-  jsymf_none <- .jcall("jdplus/math/linearfilters/FiniteFilter",
-                       "Ljdplus/math/linearfilters/FiniteFilter;",
-                       "of", rep(0, lags+1),as.integer(-lags))
-  jsymf_none <- .jcast(jsymf_none, "jdplus/math/linearfilters/IFiniteFilter")
-
-  jy <- .jcall("demetra/data/DoubleSeq",
-               "Ldemetra/data/DoubleSeq;", "of",as.numeric(y))
-
-  jasym <- lapply(0:(lags-1),
-                  function(q){
-                    if(q<leads){
-                      jsymf_none#Those values will be removed
-                    }else{
-                      jsymf
-                    }
-                  }
-  )
-  jasym <- .jarray(jasym,
-                   "jdplus/math/linearfilters/IFiniteFilter")
-
-  result <- .jcall("jdplus/math/linearfilters/FilterUtility",
-                   "Ldemetra/data/DoubleSeq;", "filter",
-                   jy,
-                   jsymf,
-                   .jnull("[Ljdplus/math/linearfilters/IFiniteFilter;"),
-                   jasym
-  )
-
-  result <- result$toArray()
-  if(leads>0){
-    result[(length(result)+1-leads):length(result)] <- NA
+  if(!is.moving_average(coefs)){
+    coefs <- moving_average(coefs, -abs(lags))
   }
+  lb = get_lower_bound(coefs)
+  ub = get_upper_bound(coefs)
+
+  DataBlock = J("jdplus.data.DataBlock")
+  jy = DataBlock$of(as.numeric(y))
+  out = DataBlock$of(as.numeric(rep(NA, jy$length() - length(coefs)+1)))
+  coefs@internal$apply(jy,
+                       out)
+  result = out$toArray()
+  result <- c(rep(NA, abs(min(lb, 0))),
+              result,
+              rep(NA, abs(max(ub, 0))))
   if(is.ts(y))
     result <- ts(result,start = start(y), frequency = frequency(y))
   result
 }
+# jasym_filter.default <- function(y, coefs, lags){
+#   coefs <- removeTrailingZeroOrNA(coefs)
+#   leads <- length(coefs)-(lags+1)
+#   jsymf <- .jcall("jdplus/math/linearfilters/FiniteFilter",
+#                   "Ljdplus/math/linearfilters/FiniteFilter;",
+#                   "of", coefs,as.integer(-lags))
+#   jsymf <- .jcast(jsymf, "jdplus/math/linearfilters/IFiniteFilter")
+#   jsymf_none <- .jcall("jdplus/math/linearfilters/FiniteFilter",
+#                        "Ljdplus/math/linearfilters/FiniteFilter;",
+#                        "of", rep(0, lags+1),as.integer(-lags))
+#   jsymf_none <- .jcast(jsymf_none, "jdplus/math/linearfilters/IFiniteFilter")
+#
+#   jy <- .jcall("demetra/data/DoubleSeq",
+#                "Ldemetra/data/DoubleSeq;", "of",as.numeric(y))
+#
+#   jasym <- lapply(0:(lags-1),
+#                   function(q){
+#                     if(q<leads){
+#                       jsymf_none#Those values will be removed
+#                     }else{
+#                       jsymf
+#                     }
+#                   }
+#   )
+#   jasym <- .jarray(jasym,
+#                    "jdplus/math/linearfilters/IFiniteFilter")
+#
+#   result <- .jcall("jdplus/math/linearfilters/FilterUtility",
+#                    "Ldemetra/data/DoubleSeq;", "filter",
+#                    jy,
+#                    jsymf,
+#                    .jnull("[Ljdplus/math/linearfilters/IFiniteFilter;"),
+#                    jasym
+#   )
+#
+#   result <- result$toArray()
+#   if(leads>0){
+#     result[(length(result)+1-leads):length(result)] <- NA
+#   }
+#   if(is.ts(y))
+#     result <- ts(result,start = start(y), frequency = frequency(y))
+#   result
+# }
 #' @export
 jasym_filter.matrix <- function(y, coefs, lags){
   result <- y
