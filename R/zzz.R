5c3a51faa820a7766efdcf15f62cd78b053d79b7
AQLT <alain.quartier@yahoo.fr>
2021-11-10 11:24:16 +0100

    rjd3toolkit imports

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,15 @@
 #' @import rJava
 #' @importFrom graphics axis lines plot matplot
 #' @importFrom stats frequency ts
+#' @importFrom rjd3toolkit .JD3_ENV
 NULL
 
 jd_clobj <- NULL
+matrix_jd2r<-NULL
+matrix_r2jd<-NULL
+ts_jd2r <- NULL
+ts_r2jd <- NULL
+tsdomain_r2jd<-NULL
 
 .onLoad <- function(libname, pkgname) {
   # For debugts_ging: to see if Jars are effectively loaded
@@ -22,6 +28,12 @@ jd_clobj <- NULL
 
   # often used while calling java
   jd_clobj <<- .jcall("java/lang/Class", "Ljava/lang/Class;", "forName", "java.lang.Object")
+
+  matrix_jd2r<<-.JD3_ENV$matrix_jd2r
+  matrix_r2jd<<-.JD3_ENV$matrix_r2jd
+  ts_jd2r<<-.JD3_ENV$ts_jd2r
+  ts_r2jd<<-.JD3_ENV$ts_r2jd
+  tsdomain_r2jd<<-.JD3_ENV$tsdomain_r2jd
 }
 
 .onAttach <- function( libname , pkgname ){
