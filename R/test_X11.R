72078431d65f8310aa3e1cc784dca2dcf93666ac
AQLT <alain.quartier@yahoo.fr>
2022-12-08 20:12:08 +0100

    suite tests

--- R/test_X11.R
+++ R/test_X11.R
@@ -15,20 +15,17 @@ M3X3_s <- rjd3filters:::finite_filters(moving_average(M3X3[,3],-2),
 M3X3_s = to_seasonal(M3X3_s, 12)
 as.matrix(M3X3_s) |>  rownames()
 s_1 = M3X3_s * si_1
+s_2 = M3X3_s * finite_filters(si_1)
+e1 = M3X3_s
+e2 = finite_filters(si_1)
 mm_s_norm = finite_filters((1 - M2X12), rfilters = lapply(1:6, function(i) (1 - M2X12)*moving_average(1, lags = -i)))
-s_1_norm = mm_s_norm * s_1
+e1 = mm_s_norm
+e2 = s_1
+s_1_norm = mm_s_norm * s_2
 sa_1 <- 1 - s_1_norm
 sa_1*y
 
-as.matrix(sa_1) |> round(3) |> View()
-henderson_mm_coef = lp_filter(horizon = 6)$
-  filters.coef
-henderson_mm = rjd3filters:::finite_filters(
-  moving_average(henderson_mm_coef[,"q=6"],-6),
-  lapply(6:1, function(i) moving_average(henderson_mm_coef[,i],-6)),
-)
-e1 = henderson_mm
-e2 = sa_1
+henderson_mm = finite_filters(lp_filter(horizon = 6,ic = 3.5))
 tc_2 <- henderson_mm * sa_1
 si_2 <- 1 - tc_2
 M5 <- moving_average(rep(1/5, 5), lags = -2)
@@ -37,3 +34,19 @@ s_2 = M5X5_seasonal * si_2
 s_2_norm = mm_s_norm * s_2
 sa_2 <- 1 - s_2_norm
 tc_f <- henderson_mm * sa_2
+tc_f * y
+tmp1 = sa_2* y
+tmp2 = x11(y = y, trend.coefs = lp_filter(horizon = 6,ic = 3.5)$filters.coef,extreme.lsig = 300, extreme.usig = 400, mul = FALSE)
+tmp1 - tmp2$decomposition[,"sa"]
+plot(tmp1)
+lines(tmp2$decomposition[,"sa"], col = "red")
+
+# IC-ratio varie ici
+tmp = rjd3x13::x11(y, rjd3x13::spec_x11_default() |>
+               rjd3x13::set_x11(mode = "Additive",
+                                seasonal.filter = "S3X5",
+                                henderson.filter = 13,
+                                lsigma = 3000,
+                                usigma = 4000))
+tmp2$decomposition[,"sa"]
+tmp$d11
