c8adbc6daac4dfde17e5e88b49ec814910869cd8
AQLT <alain.quartier@yahoo.fr>
2022-12-09 16:13:14 +0100

    test

--- R/test_X11.R
+++ R/test_X11.R
@@ -1,53 +1,117 @@
 library(rjd3filters)
 y = rjd3toolkit::ABS$X0.2.09.10.M
+
+# wk <- RJDemetra::new_workspace()
+# RJDemetra::new_multiprocessing(wk, "sa1")
+# RJDemetra::add_sa_item(wk, "sa1", RJDemetra::x13(y, spec = "X11"), "X13")
+# RJDemetra::save_workspace(wk, "workspace.xml")
+wk <- RJDemetra::load_workspace("workspace.xml")
+RJDemetra::compute(wk)
+mod <- RJDemetra::get_jmodel(wk)[[1]][[1]]
+extract <- function(id) {
+  RJDemetra::get_indicators(mod, sprintf("decomposition.%s", id))[[1]]
+}
+compare <- function(x, id){
+  res = ts.intersect(x, extract(id))
+  all.equal(res[,1], res[,2])
+}
+# extract <- function(x, id){
+#   ts(rjd3toolkit:::proc_vector(x$java, id),
+#      end = end(y), frequency = frequency(y))
+# }
+x11_step = x11(y = y, trend.coefs = lp_filter(horizon = 6,ic = 3.5)$filters.coef,extreme.lsig = 300, extreme.usig = 400, mul = FALSE)
+waldo::compare(y, extract("b1"))
+
 e1 <- moving_average(rep(1,12), lags = -6)
 e1 <- e1/sum(e1)
 e2 <- moving_average(rep(1/12, 12), lags = -5)
 # used to have the 1rst estimate of the trend
 tc_1 <- M2X12 <- (e1 + e2)/2
 coef(M2X12) |> round(3)
+compare(M2X12 * y, "b2")
 si_1 <- 1 - tc_1
+compare(si_1 * y, "b3")
 
 M3X3 <- rjd3filters::macurves("S3x3")
 M3X3_s <- rjd3filters:::finite_filters(moving_average(M3X3[,3],-2),
-                                      list(moving_average(M3X3[,1], -2),
-                                           moving_average(M3X3[,2], -2)),first_to_last = T)
+                                       list(moving_average(M3X3[,1], -2),
+                                            moving_average(M3X3[,2], -2)),first_to_last = T)
 M3X3_s = to_seasonal(M3X3_s, 12)
 as.matrix(M3X3_s) |>  rownames()
 s_1 = M3X3_s * si_1
 s_2 = M3X3_s * finite_filters(si_1)
-e1 = M3X3_s
-e2 = finite_filters(si_1)
-mm_s_norm = finite_filters((1 - M2X12), rfilters = lapply(1:6, function(i) (1 - M2X12)*moving_average(1, lags = -i)))
-e1 = mm_s_norm
-e2 = s_1
+waldo::compare(M3X3_s * (si_1 * y), s_2 * y)
+AQLTools::hc_stocks(ts.union(M3X3_s * (si_1 * y), s_2 * y))
 
-s_1_norm = mm_s_norm * s_2
+compare( x = M3X3_s * (si_1 * y), "b5")
+
+
+AQLTools::hc_stocks(ts.union(s_1 * y, extract("b5")))
+
+mm_s_norm = finite_filters((1 - M2X12), rfilters = lapply(1:6, function(i) (1 - M2X12)*moving_average(1, lags = -i) #* moving_average(1, lags = -12)
+))
+mm_s_norm = finite_filters((1 - M2X12), rfilters = lapply(1:6, function(i) (1 - M2X12) * moving_average(1, lags = -12-i)
+))
+s_1_norm = (1 - M2X12) * s_2
+(1 - M2X12) * s_2 * y - (1 - M2X12) * (s_2 * y)
+s_2_norm = mm_s_norm * s_2
+finite_filters(moving_average(rep(1/3,3), -1),
+               rfilters = moving_average(c(1/3,1/3,NA), -1))
+round(as.matrix(s_1_norm),3) |> View()
+round(as.matrix(s_2),3) |> View()
+
+compare(s_1_norm * y, "b5")
+waldo::compare(s_1_norm * y, s_2_norm * y)
+s_2_norm
+AQLTools::hc_stocks(ts.union(s_1_norm * y, extract("b5")))
 sa_1 <- 1 - s_1_norm
 sa_1*y
 
 henderson_mm = finite_filters(lp_filter(horizon = 6,ic = 3.5))
 tc_2 <- henderson_mm * sa_1
+tc_2*y
+
+rjd3x13::x13()
+
 si_2 <- 1 - tc_2
-M5 <- moving_average(rep(1/5, 5), lags = -2)
-M5X5_seasonal <- to_seasonal(M5 * M5, 12)
-s_2 = M5X5_seasonal * si_2
+si_2*y
+
+M3X5 <- rjd3filters::macurves("S3x5")
+M3X5_s <- rjd3filters:::finite_filters(moving_average(M3X5[,4],-3),
+                                       list(moving_average(M3X5[,1], -3),
+                                            moving_average(M3X5[,2], -3),
+                                            moving_average(M3X5[,3], -3)),
+                                       first_to_last = T)
+M3X5_s = to_seasonal(M3X5_s, 12)
+s_2 = M3X5_s * si_2
+dim(as.matrix(si_2))
+dim(as.matrix(s_2)) #pair, pb
+e1 = M3X5_s
+e2 = si_2
+M3X5_s * y
+si_2*y
 s_2_norm = mm_s_norm * s_2
 sa_2 <- 1 - s_2_norm
 tc_f <- henderson_mm * sa_2
 tc_f * y
 tmp1 = sa_2* y
-tmp2 = x11(y = y, trend.coefs = lp_filter(horizon = 6,ic = 3.5)$filters.coef,extreme.lsig = 300, extreme.usig = 400, mul = FALSE)
+
 tmp1 - tmp2$decomposition[,"sa"]
 plot(tmp1)
 lines(tmp2$decomposition[,"sa"], col = "red")
 
 # IC-ratio varie ici
 tmp = rjd3x13::x11(y, rjd3x13::spec_x11_default() |>
-               rjd3x13::set_x11(mode = "Additive",
-                                seasonal.filter = "S3X5",
-                                henderson.filter = 13,
-                                lsigma = 3000,
-                                usigma = 4000))
+                     rjd3x13::set_x11(mode = "Additive",
+                                      seasonal.filter = "S3X5",
+                                      henderson.filter = 13,
+                                      lsigma = 3000,
+                                      usigma = 4000))
 tmp2$decomposition[,"sa"]
 tmp$d11
+
+rjd3x13::x13()
+
+rjd3modelling::modelling_context()
+
+
