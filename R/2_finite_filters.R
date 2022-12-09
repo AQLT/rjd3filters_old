72e982e69274b7bc11b6bfadb5c9c30a2ee43a44
AQLT <alain.quartier@yahoo.fr>
2022-12-09 16:34:29 +0100

    jd2finitefilters

--- R/2_finite_filters.R
+++ R/2_finite_filters.R
@@ -76,6 +76,15 @@ finite_filters.FiniteFilters <- function(sfilter,
   res
 }
 
+jd2finitefilters <- function(jf){
+  jsfilter <- .jcall(jf, "Ljdplus/math/linearfilters/SymmetricFilter;", "symmetricFilter")
+  jlfilter <- .jcall(jf, "[Ljdplus/math/linearfilters/IFiniteFilter;", "leftEndPointsFilters")
+  jrfilter <- .jcall(jf, "[Ljdplus/math/linearfilters/IFiniteFilter;", "rightEndPointsFilters")
+  finite_filters(jd2ma(jsfilter),
+                 rfilter = lapply(jrfilter, jd2ma),
+                 lfilter = rev(lapply(jlfilter, jd2ma)))
+}
+
 #' @rdname finite_filters
 #' @export
 setMethod("*",
