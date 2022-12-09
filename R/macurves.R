3121f53dd1d3964ae431f8167aea5b70c6f5a2cd
AQLT <alain.quartier@yahoo.fr>
2022-12-09 16:34:03 +0100

    macurves filters as finite_filters

--- R/macurves.R
+++ R/macurves.R
@@ -1,33 +1,22 @@
 #' Get Macurves Filters
 #'
-#' @param seas_filter the filter to extract
-#'
+#' @param seas_filter the filter to extract.
+#' @param period period of the filter.
+#' @examples
+#' macurves("S3X3")
 #' @export
-macurves <- function(seas_filter = c("S3X3", "S3X1", "S3X5", "S3X9", "S3X15")){
+macurves <- function(seas_filter = c("S3X3", "S3X1", "S3X5", "S3X9", "S3X15"), period = 12){
   seas_filter = match.arg(toupper(seas_filter),
                           c("S3X3", "S3X1", "S3X5", "S3X9", "S3X15"))
   seas_opt <- .jcall("jdplus/x11plus/SeasonalFilterOption",
                      "Ljdplus/x11plus/SeasonalFilterOption;",
                      "valueOf",
                      seas_filter)
-  P = .jcast(new( J("java.lang.Double"), as.character(12)),
+  P = .jcast(new( J("java.lang.Double"), as.character(period)),
              "java.lang.Number")
   seasFilter <- .jcall("jdplus/x11plus/X11SeasonalFiltersFactory",
                        "Ljdplus/filters/ISymmetricFiltering;",
                        "filter",
                        P, seas_opt)
-  symFilter <- .jcall(seasFilter, "Ljdplus/math/linearfilters/SymmetricFilter;", "symmetricFilter")
-  asymFilter <- .jcall(seasFilter, "[Ljdplus/math/linearfilters/IFiniteFilter;", "endPointsFilters")
-
-  coefs = c(rev(asymFilter),list(symFilter))
-  nbpoints = symFilter$length()
-  horizon = (nbpoints - 1)/2
-  coefs = sapply(coefs, function(x){
-    x = x$weightsToArray()
-    c(x,rep(0,nbpoints-length(x)))
-  })
-  rownames(coefs) <- coefficients_names(-horizon, horizon)
-  colnames(coefs) <- sprintf("q=%i", 0:(horizon))
-
-  coefs
+  jd2finitefilters(seasFilter)
 }
